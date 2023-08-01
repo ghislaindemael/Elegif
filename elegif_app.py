87262bf7192565f94c4b0995b048e9ab54185307
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-01 16:53:47 +0200

    More work on Tkinter panel

--- elegif_app.py
+++ elegif_app.py
@@ -1,119 +1,207 @@
-import os
-import sys
-import instabot
+import tkinter as tk
+from tkinter import ttk
 
-from PIL import Image
-from elegif.helper import *
-from elegif.picture import generate_pic
-from elegif.video import generate_video
 
-# Set the actions
-gen_pic = True
-share_pic_insta = False
+def create_generator_tab(tab):
+    # Title
+    title_label = ttk.Label(tab, text="Elegif Generator", font=("Helvetica", 16, "bold"))
+    title_label.pack(pady=20)
 
-gen_vid = True
-share_vid_insta = True
-share_vid_tiktok = False
+    # Title input
+    title_frame = ttk.Frame(tab)
+    title_frame.pack(pady=5, padx=50)
 
-# The poem
-lang = "##"
-name = "TestPubliInstagram"
-description = "Test de publication sur Instagram via élégif"
-inspiration = "Adopte un zombie, Magoyong"
+    title_label = ttk.Label(title_frame, text="Title :")
+    title_label.grid(row=0, column=0, padx=10)
 
-poem = read_poem_file()
-poem.append("\n- R.D -")
+    title_entry = ttk.Entry(title_frame, width=50)  # Set width to 50 characters (350 pixels)
+    title_entry.grid(row=0, column=1, padx=10, pady=5, sticky="ew")
 
-# Image properties
-i_width, i_height = 1080, 1080
-# Video properties
-width, height, fps, dur_per_line = 1080, 1920, 24, 2
-# Animation properties
-anim_type, anim_dur = "fade_in", 1
-# Colors
-video_bg_color, image_bg_color, text_color = "#FEFEFE", "#FAFAFA", "#454545"
-# Text properties
-font, text_size, intra_line_height = "ggsans-med.ttf", 50, 0.25
-# Flag properties
-flag_width, flag_height = 125, 125
-# Music properties
-music_file, volume = "music.mp3", 10
+    # Language input
+    language_frame = ttk.Frame(tab)
+    language_frame.pack(pady=5, padx=50)
 
-# The code
-image_params = [i_width, i_height]
-video_params = [width, height, fps, dur_per_line]
-anim_params = [anim_type, anim_dur]
-color_params = [video_bg_color, image_bg_color, text_color]
-text_params = [font, text_size, intra_line_height]
-flag_params = [lang, flag_width, flag_height]
-music_params = [music_file, 10]
+    language_label = ttk.Label(language_frame, text="Language :")
+    language_label.grid(row=0, column=0, padx=10)
 
-# Manage file names
-if name == "":
-    sys.exit()
+    languages = ["French", "English", "German", "Italian", "###"]  # Languages to choose from
+    language_var = tk.StringVar()
+    language_var.set("###")  # Default value is "###"
 
-pic_name = "Poème" + name
-if lang != "":
-    pic_name += f"_{lang.upper()}"
-pic_output = os.path.join("output", f"{pic_name}.png")
+    language_dropdown = ttk.Combobox(language_frame, textvariable=language_var, values=languages, state="readonly")
+    language_dropdown.grid(row=0, column=1, padx=10, pady=5, sticky="ew")
 
-vid_name = name
-if anim_type != "":
-    vid_name += "_A"
-if lang != "":
-    vid_name += f"_{lang.upper()}"
-vid_output = os.path.join("output", f"{vid_name}.mp4")
+    # Poem input
+    poem_frame = ttk.Frame(tab)
+    poem_frame.pack(pady=0, padx=50, fill=tk.X, expand=False)  # Use fill=tk.BOTH and expand=True
 
-if gen_pic:
-    # Generate the picture
-    picture = generate_pic(poem, image_params, color_params, text_params, flag_params)
-    picture.save(pic_output)
+    poem_label = ttk.Label(poem_frame, text="Poem :")
+    poem_label.grid(row=0, column=0, padx=10)  # Centered
 
-    temp_insta_image = Image.open(pic_output)
-    temp_insta_image.save("temp_insta.jpg", "JPEG")
-    temp_insta_image.close()
+    poem_entries = []  # List to store poem entry widgets
 
-    caption = description + "\n\n"
-    caption += get_inspiration_translation(lang) + inspiration + "\n\n"
-    caption += get_hashtags()
+    def add_poem_line():
+        new_poem_entry = ttk.Entry(poem_frame, width=50)  # Set width to 50 characters (350 pixels)
+        new_poem_entry.grid(row=len(poem_entries) + 1, column=0, padx=5, pady=0, sticky="ew")
+        poem_entries.append(new_poem_entry)
+        update_window_size()
 
-    if share_pic_insta:
+    def remove_poem_line():
+        if len(poem_entries) > 1:
+            last_poem_entry = poem_entries.pop()
+            last_poem_entry.destroy()
+            update_window_size()
 
-        bot = instabot.Bot()
-        username, password = load_credentials("instagram")
-        bot.login(username=username, password=password)
+    def update_window_size():
+        root.update_idletasks()  # Update the window immediately before calculating size
+        total_height = 20 * len(poem_entries) + 375
+        total_width = 400
+        root.geometry(f"{total_width}x{total_height}")
 
-        bot.upload_photo("temp_insta.jpg", caption=caption)
+    poem_entry = ttk.Entry(poem_frame, width=50)  # Set width to 50 characters (350 pixels)
+    poem_entry.grid(row=1, column=0, padx=5, pady=0, sticky="ew")
+    poem_entries.append(poem_entry)
 
-    os.remove("temp_insta.jpg")
+    # +/- Buttons
+    buttons_frame = ttk.Frame(tab)
+    buttons_frame.pack(pady=10)
 
-if gen_vid:
-    # Generate the video
-    video = generate_video(poem, video_params, anim_params, color_params, text_params, flag_params)
+    plus_button = ttk.Button(buttons_frame, text="+", command=add_poem_line)
+    plus_button.grid(row=0, column=0, padx=5)
 
-    # Add music to the video
-    video_with_music = video
-    # video_with_music = add_music(video, music_file)
+    minus_button = ttk.Button(buttons_frame, text="-", command=remove_poem_line)
+    minus_button.grid(row=0, column=1, padx=5)
 
-    # Save the video
-    video_with_music.write_videofile(vid_output, fps=fps, codec="libx264", audio_codec="aac")
+    # Extra properties section
+    extra_properties_frame = ttk.Frame(tab)
+    extra_properties_frame.pack(pady=10)
 
-    caption = description + "\n\n"
-    caption += get_inspiration_translation(lang) + inspiration + "\n\n"
-    caption += get_hashtags()
+    # Flag Size
+    flag_size_label = ttk.Label(extra_properties_frame, text="Flag Size:")
+    flag_size_label.grid(row=0, column=0, padx=5)
+    flag_size_var = tk.DoubleVar()
+    flag_size_var.set(111)
+    flag_size_entry = ttk.Entry(extra_properties_frame, textvariable=flag_size_var, width=6)
+    flag_size_entry.grid(row=0, column=1, padx=5)
 
-    if share_vid_insta:
+    # Duration per line
+    duration_label = ttk.Label(extra_properties_frame, text="Duration per Line:")
+    duration_label.grid(row=0, column=2, padx=5)
+    duration_var = tk.DoubleVar()
+    duration_var.set(2)
+    duration_entry = ttk.Entry(extra_properties_frame, textvariable=duration_var, width=6)
+    duration_entry.grid(row=0, column=3, padx=5)
 
-        print('publish to insta')
-        bot = instabot.Bot()
-        username, password = load_credentials("instagram")
-        bot.login(username=username, password=password)
+    # Animated Checkbox
+    animated_label = ttk.Label(extra_properties_frame, text="Animated")
+    animated_label.grid(row=0, column=4, padx=5)
+    animated_var = tk.BooleanVar()
+    animated_var.set(True)  # Default value is True
+    animated_checkbox = ttk.Checkbutton(extra_properties_frame, variable=animated_var)
+    animated_checkbox.grid(row=0, column=5, padx=5)
 
-        temp_vid = create_copy(vid_output)
+    # Anim Duration
+    anim_duration_label = ttk.Label(extra_properties_frame, text="Anim Dur:")
+    anim_duration_label.grid(row=1, column=4, padx=5)
+    anim_duration_var = tk.DoubleVar()
+    anim_duration_var.set(1)
+    anim_duration_entry = ttk.Entry(extra_properties_frame, textvariable=anim_duration_var, width=6)
+    anim_duration_entry.grid(row=1, column=5, padx=5)
 
-        bot.upload_video(temp_vid, caption=caption)
+    # Text Size
+    text_size_label = ttk.Label(extra_properties_frame, text="Text Size:")
+    text_size_label.grid(row=1, column=0, padx=5)
+    text_size_var = tk.DoubleVar()
+    text_size_var.set(50)
+    text_size_entry = ttk.Entry(extra_properties_frame, textvariable=text_size_var, width=6)
+    text_size_entry.grid(row=1, column=1, padx=5)
 
-        os.remove('config')
+    # Intra-line Height
+    intra_line_label = ttk.Label(extra_properties_frame, text="Intra-line Height:")
+    intra_line_label.grid(row=1, column=2, padx=5)
+    intra_line_var = tk.DoubleVar()
+    intra_line_var.set(0.25)
+    intra_line_entry = ttk.Entry(extra_properties_frame, textvariable=intra_line_var, width=6)
+    intra_line_entry.grid(row=1, column=3, padx=5)
 
-    if share_vid_tiktok:
-        print('publish to tt')
+    def extract_properties():
+        # The poem
+        lang = language_var.get()
+        name = title_entry.get()
+
+        poem = [entry.get() for entry in poem_entries]
+        poem.append("\n- R.D -")
+
+        # Image properties
+        i_width, i_height = 1080, 1080
+        # Video properties
+        width, height, fps, dur_per_line = 1080, 1920, 24, duration_var.get()
+        # Animation properties
+        anim_type, anim_dur = "fade_in" if animated_var.get() else "", anim_duration_var.get()
+        # Colors
+        video_bg_color, image_bg_color, text_color = "#FEFEFE", "#FAFAFA", "#454545"
+        # Text properties
+        font, text_size, intra_line_height = "ggsans-med.ttf",text_size_var.get(), intra_line_var.get()
+        # Flag properties
+        flag_width, flag_height = flag_size_var.get(), flag_size_var.get()
+
+        return [[i_width, i_height], [width, height, fps, dur_per_line], [anim_type, anim_dur],
+                [video_bg_color, image_bg_color, text_color], [font, text_size, intra_line_height],
+                [lang, flag_width, flag_height]
+                ]
+
+    def generate_pic():
+        properties = extract_properties()
+
+
+    def generate_vid():
+        properties = extract_properties()
+
+    # Generate Picture and Generate Video buttons
+    generate_buttons_frame = ttk.Frame(tab)
+    generate_buttons_frame.pack(pady=10)
+
+    generate_picture_button = ttk.Button(generate_buttons_frame, text="Generate Picture", command=generate_pic)
+    generate_picture_button.pack(side=tk.LEFT, padx=5)
+
+    generate_video_button = ttk.Button(generate_buttons_frame, text="Generate Video", command=generate_vid)
+    generate_video_button.pack(side=tk.LEFT, padx=5)
+
+    update_window_size()
+
+
+
+
+
+def create_publisher_tab(tab):
+    # Add widgets for poem publishing
+    pass
+
+
+def create_window():
+    global root  # Make root a global variable so it can be accessed from other functions
+    root = tk.Tk()
+    root.title("Poem Editor")
+    root.geometry("400x350")
+    root.resizable(False, False)
+
+    notebook = ttk.Notebook(root)
+    notebook.pack(fill=tk.BOTH, expand=True)
+
+    generator_tab = ttk.Frame(notebook)
+    publisher_tab = ttk.Frame(notebook)
+
+    notebook.add(generator_tab, text="Generator")
+    notebook.add(publisher_tab, text="Publisher")
+
+    create_generator_tab(generator_tab)
+    create_publisher_tab(publisher_tab)
+
+    root.mainloop()
+
+
+
+
+
+if __name__ == "__main__":
+    create_window()
---
+++ old_app.py
@@ -0,0 +1,117 @@
+import os
+import sys
+import instabot
+
+from PIL import Image
+from elegif.helper import *
+from elegif.picture import generate_pic
+from elegif.video import generate_video
+
+# Set the actions
+gen_pic = True
+share_pic_insta = False
+
+gen_vid = True
+share_vid_insta = True
+share_vid_tiktok = False
+
+# The poem
+lang = "##"
+name = "TestPubliInstagram"
+description = "Test de publication sur Instagram via élégif"
+inspiration = "Adopte un zombie, Magoyong"
+
+poem = read_poem_file()
+poem.append("\n- R.D -")
+
+# Image properties
+i_width, i_height = 1080, 1080
+# Video properties
+width, height, fps, dur_per_line = 1080, 1920, 24, 2
+# Animation properties
+anim_type, anim_dur = "fade_in", 1
+# Colors
+video_bg_color, image_bg_color, text_color = "#FEFEFE", "#FAFAFA", "#454545"
+# Text properties
+font, text_size, intra_line_height = "ggsans-med.ttf", 50, 0.25
+# Flag properties
+flag_width, flag_height = 120, 120
+
+
+# The code
+image_params = [i_width, i_height]
+video_params = [width, height, fps, dur_per_line]
+anim_params = [anim_type, anim_dur]
+color_params = [video_bg_color, image_bg_color, text_color]
+text_params = [font, text_size, intra_line_height]
+flag_params = [lang, flag_width, flag_height]
+
+# Manage file names
+if name == "":
+    sys.exit()
+
+pic_name = "Poème" + name
+if lang != "":
+    pic_name += f"_{lang.upper()}"
+pic_output = os.path.join("output", f"{pic_name}.png")
+
+vid_name = name
+if anim_type != "":
+    vid_name += "_A"
+if lang != "":
+    vid_name += f"_{lang.upper()}"
+vid_output = os.path.join("output", f"{vid_name}.mp4")
+
+if gen_pic:
+    # Generate the picture
+    picture = generate_pic(poem, image_params, color_params, text_params, flag_params)
+    picture.save(pic_output)
+
+    temp_insta_image = Image.open(pic_output)
+    temp_insta_image.save("temp_insta.jpg", "JPEG")
+    temp_insta_image.close()
+
+    caption = description + "\n\n"
+    caption += get_inspiration_translation(lang) + inspiration + "\n\n"
+    caption += get_hashtags()
+
+    if share_pic_insta:
+
+        bot = instabot.Bot()
+        username, password = load_credentials("instagram")
+        bot.login(username=username, password=password)
+
+        bot.upload_photo("temp_insta.jpg", caption=caption)
+
+    os.remove("temp_insta.jpg")
+
+if gen_vid:
+    # Generate the video
+    video = generate_video(poem, video_params, anim_params, color_params, text_params, flag_params)
+
+    # Add music to the video
+    video_with_music = video
+    # video_with_music = add_music(video, music_file)
+
+    # Save the video
+    video_with_music.write_videofile(vid_output, fps=fps, codec="libx264", audio_codec="aac")
+
+    caption = description + "\n\n"
+    caption += get_inspiration_translation(lang) + inspiration + "\n\n"
+    caption += get_hashtags()
+
+    if share_vid_insta:
+
+        print('publish to insta')
+        bot = instabot.Bot()
+        username, password = load_credentials("instagram")
+        bot.login(username=username, password=password)
+
+        temp_vid = create_copy(vid_output)
+
+        bot.upload_video(temp_vid, caption=caption)
+
+        os.remove('config')
+
+    if share_vid_tiktok:
+        print('publish to tt')
